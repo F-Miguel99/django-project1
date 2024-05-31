5c1716c511b9542e36c5d3e217bc613b47269af9
Filipe Miguel <filipe.miguel99@gmail.com>
2024-05-31 16:14:54 +0100

    injectando dados na view Home

--- recipes/views.py
+++ recipes/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # from django.shortcuts import render
 from django.urls import path
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     # return HttpResponse('Home Page')
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context ={
+        # 'name': 'Filipe Miguel',
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Filipe Miguel',
+        # 'name': 'Filipe Miguel',
+        'recipe': make_recipe(),
     })
 
