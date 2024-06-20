e8f18317e16e695d7221e39d042d3060b7781a92
Filipe Miguel <filipe.miguel99@gmail.com>
2024-06-20 09:15:53 +0100

    Adding recipes through admin

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 # from recipes.views import home  
 from . import views
-
+app_name = 'recipes'
 
 urlpatterns = [
     # path('sobre/', sobre),
     #path('contato/', contato),
     # path('', home),
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    # path('', views.home),
+    # path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
     
 ]
